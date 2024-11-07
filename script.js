5d6bf6698e35afb396b9723063ab9d14d43027f5
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-11-07 03:20:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let sequence = [0, 1];
 for (let i = 2; i <= num; i++) {
 sequence.push(sequence[i - 1] + sequence[i - 2]);
 }
-        return sequence;
+    return sequence;
     };
 
     const fibSequence = fibonacci(numberInput);
