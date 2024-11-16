62b583170657f1ede01f666eb97ed1b304a0e57a
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-11-16 20:55:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ beginAtZero: true
     // Stworzenie nowego wykresu, jeśli dane są poprawne
     if (fibSequence.every(num => Number.isFinite(num))) {
         window.fibChart = new Chart(chartCanvas, config);
-    } else {
+} else {
 result.textContent = "Invalid data for chart generation.";
 }
 });
